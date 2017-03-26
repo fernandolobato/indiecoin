1c807044f9da64b2134c8adbe4619d5719c513fc
fernandolobato <lobato.meeser.fernando@hotmail.com>
2017-03-25 18:27:39 -0600

    documented transactions

--- indiecoin/blockchain/transaction.py
+++ indiecoin/blockchain/transaction.py
@@ -8,12 +8,57 @@ from .database import Database
 
 
 class Transaction(object):
+    """ Class Representing a Transaction instance.
 
+        This class will serve as an interphase to handel transactions. It
+        can recieve transaction information an structure it. Validates
+        information inside a transaction and can serialize object or
+        write to database.
+
+        Attributes
+        -----------
+            hash : string
+                sha256 hash of transaction
+            block_hash : string
+                sha256 hash of block to which transaction belongs to.
+            num_inputs: int
+                number of inputs inside a transaction
+            num_outputs: int
+                number of outputs inside a transaction
+            timestamp: string
+                UNIX timestamp of when transaction was created
+            is_coinbase: boolean
+                boolean value representing if transaction is coinbase
+            tx_inputs: list[]
+                list of indiecoin.blockchain.transaction.TransactionInput objects
+            tx_outputs: list[]
+                list of indiecoin.blockchain.transaction.TransactionOutput objects
+            __database: indiecoin.blockchain.transaction.database
+                instance of database on which to perform lookups and writeups.
+
+        Raises
+        -------
+            AssertionError('Transaction is not valid'):
+                If transaction information is not valid
+                check is_valid() function.
+    """
     def __init__(self, *args, **kwargs):
+        """ Transaction Contructor
+
+            Checks if a specific instance of a database was sent through
+            constructor. If not, initialized a new instance. If no hash has
+            been sent it calculates it with all other information.
+
+            For each tx_input and tx_out recieved, checkes if they are already
+            an instance of a TransactionInput or TransactionOutput object. If
+            they were just sent a dictionary, a new instance of each object is
+            created.
+
+        """
         self.hash = kwargs['hash']
         self.block_hash = kwargs['block_hash']
-        self.num_inputs = kwargs['num_inputs']
-        self.num_outputs = kwargs['num_outputs']
+        self.num_inputs = int(kwargs['num_inputs'])
+        self.num_outputs = int(kwargs['num_outputs'])
         self.timestamp = kwargs['timestamp']
         self.is_coinbase = True if int(kwargs['is_coinbase']) == 1 else False
         self.is_orphan = True if int(kwargs['is_orphan']) == 1 else False
@@ -28,16 +73,29 @@ class Transaction(object):
         if self.__database is None:
             self.__database = Database()
 
-        if len(self.hash) < 64:
+        if len(self.hash) < 64:  # Size of sha256 digest as a string
             self.hash = self.valid_hash()
 
         if not self.is_valid():
             raise AssertionError('Transaction not valid')
 
     def set_block_hash(self, block_hash):
+        """ Sets a block hash on a transaction. This will be called
+            once the miner has found a correct hash for the block
+            so each transaction can reference it later.
+        """
         self.__block_hash = block_hash
 
     def valid_hash(self):
+        """ Calculates the valid hash for this transaction.
+
+            Takes data from serialize(), removes hash and block
+            hash.
+
+            Returns
+            -------
+                sha256 representation of object
+        """
         data = self.serialize()
 
         [data.pop(field, None) for field in ['block_hash', 'hash']]
@@ -45,10 +103,27 @@ class Transaction(object):
         return sha256(str(data))
 
     def is_valid(self):
-        """
+        """ Checks that a transaction is valid.
+
+            Performs the following checks:
+
+                - The sum of inputs most be less than or equal to
+                the sum of outputs (no overspending)
+
+                - All tx_outputs referenced in each input most not
+                be spent yet.
+
+                - The signature for each input presented most match
+                the public key stored in the output it represents.
         """
         input_total = 0
         output_total = 0
+
+        if self.num_inputs != len(self.tx_inputs):
+            return False
+
+        if self.num_outputs != len(self.tx_outputs):
+            return False
 
         for tx_input in self.tx_inputs:
             input_total += tx_input.amount
@@ -67,11 +142,33 @@ class Transaction(object):
         return True
 
     def save(self):
-        """
+        """ Saves a transaction along with its transaction inputs
+            and outputs to local database.
+
+            Returns
+            -------
+                id_new_object: int
+                    In the case the object is saved succesfully.
+                None: None
+                    In the case the object could not be saved.
         """
         return self.__database.save_transaction(self.serialize())
 
     def serialize(self):
+        """ Serializes a transaction object into a dictionary representation.
+
+            To serialize each input and output we call its serialize method.
+
+            Returns
+            -------
+                data : dict
+                    dictionary representation of objects attributtes.
+
+            Notes
+            ------
+                self.database is an attribute that is not used for
+                serialization hence removed.
+        """
         data = remove_dict_prefix(self.__dict__, '_Transaction')
 
         data['tx_outputs'] = [tx.serialize() for tx in data['tx_outputs']]
@@ -84,6 +181,8 @@ class Transaction(object):
         return data
 
     def to_json(self):
+        """ Returns a strins representation in JSON of Transaction object.
+        """
         return json.dumps(self.serialize())
 
     def __str__(self):
@@ -95,18 +194,27 @@ class TransactionInput(object):
 
         Attributes
         ----------
-            signature:
+            signature: string
+                hexadecimal string representation of an ecdsa signature.
+                This signature unlucks the transaction output referenced
+                in this input.
 
-            hash:
+            hash_transaction: string
+                sha256 hash representing the id of a past transaction.
 
-            prev_out_index:
-
-            database:
-
-            prev_tx:
+            prev_out_index: int
+                Index pointing to which transaction_output of transaction
+                is being referenced in this TransactionInput.
+            __database: indiecoin.blockchain.transaction.database
+                instance of database on which to perform lookups and writeups.
+            __prev_tx: indiecoin.blockchain.transaction.Transaction
+                transaction referenced by hash_transaction
     """
     def __init__(self, *args, **kwargs):
-        """
+        """ Constructor for TransactionInput
+
+            Looks up previous transaction using hash provided in
+            constructor.
         """
         self.signature = kwargs['signature']
         self.hash_transaction = kwargs['hash_transaction']
@@ -187,15 +295,16 @@ class TransactionOutput(object):
         Attributes
         ----------
             amount : int
-
+                Value of transaction output.
             public_key_owner : string
-
+                String hexadecimal representation of an ecdsa public
+                key representing the entity who can spend transaction.
             unspent: boolean
-
+                Value indicating if transaction has been spent.
     """
 
     def __init__(self, *args, **kwargs):
-        """
+        """ Constructor for TransactionOutput
         """
         self.amount = int(kwargs['amount'])
         self.public_key_owner = kwargs['public_key_owner']
@@ -206,7 +315,17 @@ class TransactionOutput(object):
             self.__database = Database()
 
     def serialize(self):
-        """
+        """ Serializes a transaction object into a dictionary representation.
+
+            Returns
+            -------
+                data : dict
+                    dictionary representation of objects attributtes.
+
+            Notes
+            ------
+                self.database is an attribute that is not used for
+                serialization hence removed.
         """
         data = remove_dict_prefix(self.__dict__, '_TransactionOutput')
         fields = ['__database']
@@ -214,6 +333,9 @@ class TransactionOutput(object):
         return data
 
     def to_json(self):
+        """ Returns a string JSON representation of this objects
+            serialization.
+        """
         return json.dumps(self.serialize())
 
     def __str__(self):
@@ -226,10 +348,22 @@ class Database(Database):
     """
 
     def __init__(self, file_name=None):
+        """ Constructor for transaction.Database calls super
+            constructor.
+        """
         super(Database, self).__init__(file_name=file_name)
 
     def get_transaction(self, hash_trans):
-        """
+        """ Retrieves a transaction through a hash identifier.
+
+            After getting transaction it queries the Inputs and
+            Outpus. Turn each dictionary data into an object and
+            add them to Transaction instance.
+
+            Returns
+            -------
+                transaction: indiecoin.blockchain.transaction.Transaction
+                    transaction instance
         """
         transaction = self.__get_transaction(hash_trans)
 
@@ -245,7 +379,13 @@ class Database(Database):
         return Transaction(**transaction)
 
     def get_block_transactions(self, block_hash):
-        """
+        """ Gets all the transactions belonging to a block
+
+            Returns
+            -------
+            transactions: list
+                list of indiecoin.blockchain.transaction.Transaction
+
         """
         transactions = self.__get_transactions_block(block_hash)
 
@@ -256,7 +396,11 @@ class Database(Database):
         return [Transaction(**tx) for tx in transactions]
 
     def save_transaction(self, transaction):
-        """
+        """ Saves a transaction object to the database.
+
+            After saving transaction object it gets its
+            newely created id and uses it to save each
+            transaction input and output.
         """
         if not self.__get_transaction(transaction['hash']):
             inputs = transaction.pop('tx_inputs')
