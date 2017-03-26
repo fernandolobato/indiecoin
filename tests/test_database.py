bb0db8ed25f1542bf58e8c9bc4a7893a71106b68
fernandolobato <lobato.meeser.fernando@hotmail.com>
2017-03-25 18:41:49 -0600

    Tests for database

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,25 +4,23 @@ import sqlite3
 import os
 
 from context import indiecoin, GENESIS_BLOCK_HASH
-from indiecoin.blockchain.database import Database
-from indiecoin.util import hash
 from indiecoin.util import default_data_directory
 
 
 class DatabaseTestCase(unittest.TestCase):
     """ Test the functionality for interacting with the database.
-        
+
         Creates a new database called test_database to test that
         when running the program all the tables are generated,
-        the genesis block is inserted into the database.   
- 
+        the genesis block is inserted into the database.
+
     """
     def setUp(self):
         """ Create database object with different file_name. Open connection to database.
         """
         self.file_name = 'test_database'
         self.path = os.path.join(default_data_directory(), self.file_name)
-        self.database = Database(file_name=self.file_name)
+        self.database = indiecoin.blockchain.database.Database(file_name=self.file_name)
         self.connection = sqlite3.connect(self.path)
         self.genesis_block = GENESIS_BLOCK_HASH
 
@@ -40,7 +38,6 @@ class DatabaseTestCase(unittest.TestCase):
         cursor.execute(sql)
         data = cursor.fetchall()
         self.assertEqual(len(data), 1)
-        
 
 
 if __name__ == '__main__':
